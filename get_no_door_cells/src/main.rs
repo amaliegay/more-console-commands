10f4923aca8970fdba9932db42801c6bdf2101e6
Amalie <amaliegay@outlook.com>
2023-06-13 04:47:06 -0400

    get no door update?

--- get_no_door_cells/src/main.rs
+++ get_no_door_cells/src/main.rs
@@ -1,25 +1,23 @@
-fn main() {
-    get_no_door_cells();
+use tes3::esp::{Plugin, Door};
+fn main() -> std::io::Result<()> {
+    let mut plugin = Plugin::new();
+    plugin.load_path("../Morrowind.esm")?;
+
+    for object in plugin.objects_of_type::<Door>() {
+        if object.id == "fargoth" {
+            println!("{object:#?}");
+        }
+    }
+
+    Ok(())
 }
 
 fn get_no_door_cells() {
-    // let cells = tes3.dataHandler.nonDynamicData.cells
-    use std::collections::HashMap;
-
-    let mut objectType = HashMap::new();
-
-    objectType.insert(String::from("door"), 1380929348);
-
-    struct Tes3Cell {
-        isInterior: bool,
-        iterateReferences: fn(),
-    }
-   let cells: &[Tes3Cell];
-    for cell in &cells {
-        if cell.isInterior {
-            for door in cell:iterateReferences(objectType.door) {
-                if door.destination {
-                    println!(cell.editorName);
+    for &cell in &tes3.dataHandler.nonDynamicData.cells {
+        if &cell.isInterior {
+            for &door in &cell.iterateReferences(&tes3.objectType.door) {
+                if &door.destination {
+                    println!("{}", &cell.editorName);
                     break;
                 }
             }
