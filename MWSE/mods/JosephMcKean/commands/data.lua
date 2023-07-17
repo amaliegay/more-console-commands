cb5fb9809c83ce63b77cb1c5b8d6aeaa6ed521a4
Amalie <amaliegay@outlook.com>
2023-07-17 17:24:54 +0800

    fixed lookup command filter

--- MWSE/mods/JosephMcKean/commands/data.lua
+++ MWSE/mods/JosephMcKean/commands/data.lua
@@ -834,19 +834,27 @@ data.commands = {
 			{ index = 1, metavar = "objecttype", required = false, help = "the type of the object to look up" },
 		},
 		callback = function(argv)
+			local isCellType = argv[#argv] == "cell"
 			local objectType = getObjectType(argv[#argv])
-			if objectType then table.remove(argv, #argv) end
+			if isCellType or objectType then
+				log:trace("%s is objectType", argv[#argv])
+				table.remove(argv, #argv)
+			end
 			local name = argv and not table.empty(argv) and table.concat(argv, " ") or nil
 			if not name then return end
 			local lookUpObjs = {}
-			for object in tes3.iterateObjects(objectType) do
-				local obj = object ---@cast obj tes3object|tes3npc
-				local objId = obj.id and obj.id:lower()
-				local objName = obj.name and obj.name:lower()
-				if objId:find(name) or (objName and objName:find(name)) then table.insert(lookUpObjs, obj) end
+			if objectType then
+				for object in tes3.iterateObjects(objectType) do
+					local obj = object ---@cast obj tes3object|tes3npc
+					local objId = obj.id and obj.id:lower()
+					local objName = obj.name and obj.name:lower()
+					if objId:find(name) or (objName and objName:find(name)) then table.insert(lookUpObjs, obj) end
+				end
 			end
-			local nonDynamicData = tes3.dataHandler.nonDynamicData
-			for _, cell in ipairs(nonDynamicData.cells) do if cell.id:lower():find(name) then table.insert(lookUpObjs, cell) end end
+			if isCellType then
+				local nonDynamicData = tes3.dataHandler.nonDynamicData
+				for _, cell in ipairs(nonDynamicData.cells) do if cell.id:lower():find(name) then table.insert(lookUpObjs, cell) end end
+			end
 			if table.empty(lookUpObjs) then
 				tes3ui.log("No matching information")
 			else
